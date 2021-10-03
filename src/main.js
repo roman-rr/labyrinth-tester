ecf59d1f04a9a6f7ad659925709313e4319f7e8b
roman-rr <romwtb@gmail.com>
2021-10-03 13:26:19 +0700

    good begining

--- src/main.js
+++ src/main.js
@@ -1,67 +1,105 @@
-// Не забудьте перед отправкой изменить в module.exports = function main(game, start) {
-// Не деструктурируйте game, ваше решение не будет проходить тесты.
-export default async function main(game, start) {
+export default async function main(node, start) {
     let state;
     let isFinished;
     let currentX = start.x;
     let currentY = start.y;
+    let steps = [];
 
-    await game.right(currentX, currentY);
-    currentX += 1;
-
-
-    while (!isFinished) {
-        state = await game.state(currentX, currentY);
-        isFinished = state.finish;
-        // console.log('state', state);
-        // console.log('finished ?', isFinished.finish);
-
-        if (state.bottom) {
-            await game.down(currentX, currentY);
-            currentY += 1;
-            continue;
-        }
-
-        if (state.right) {
-            await game.right(currentX, currentY);
+    let doStep = async(direction) => {
+        if (direction === 'right') {
+            await node.right(currentX, currentY);
             currentX += 1;
-            continue;
         } 
 
-
-        // if (state.right) {
-        //     await game.right(currentX, currentY);
-        //     currentX += 1;
-        //     continue;
-        // } 
-
-        // if (state.bottom) {
-        //     await game.down(currentX, currentY);
-        //     currentY += 1;
-        //     continue;
-        // }
+        if (direction === 'bottom') {
+            await node.down(currentX, currentY);
+            currentY += 1;
+        }
         
-        // if (state.left) {
-        //     await game.left(currentX, currentY);
-        //     currentX -= 1;
-        //     continue;
-        // } 
-        // if (state.top) {
-        //     await game.up(currentX, currentY);
-        //     currentY -= 1;
-        //     continue;
-        // } 
+        if (direction === 'left') {
+            await node.left(currentX, currentY);
+            currentX -= 1;
+        } 
+
+        if (direction === 'top') {
+            await node.up(currentX, currentY);
+            currentY -= 1;
+        } 
+
+        steps.push(`${currentX},${currentY}`);
+    }
+
+    const canGoDirecation = (dir, state) => {
+        let value = false;
+        if (dir === 'left') {
+            if (state.left && !steps.includes(`${currentX - 1},${currentY}`))
+                value = true;
+        } 
+        if (dir === 'right') {
+            if (state.right && !steps.includes(`${currentX + 1},${currentY}`)) 
+                value = true;
+        }  
+        if (dir === 'top') {
+            if (state.top && !steps.includes(`${currentX},${currentY - 1}`)) 
+                value = true;
+        }
+        if (dir === 'bottom') {
+            if (state.bottom && !steps.includes(`${currentX},${currentY + 1}`)) 
+                value = true;
+        }
+        return value;
+    }
+
+    const waysToGo = (state) => {
+        let arr = [
+            canGoDirecation('right', state), 
+            canGoDirecation('left', state), 
+            canGoDirecation('top', state), 
+            canGoDirecation('bottom', state)
+        ];
+        return arr.filter(Boolean).length;
+    };
+
+    await doStep('right');
+
+    while (!isFinished) {
+        state = await node.state(currentX, currentY);
+
+        console.log('ways to go', waysToGo(state));
+        if (waysToGo(state)) {
+
+            if (canGoDirecation('bottom', state)) {
+                console.log('go forward bottom');
+                await doStep('bottom');
+                continue;
+            }
+
+            if (canGoDirecation('right', state)) {
+                console.log('go forward right');
+                await doStep('right');
+                continue;
+            }
+
+            if (canGoDirecation('left', state)) {
+                console.log('go forward left');
+                await doStep('left');
+                continue;
+            }
+
+            if (canGoDirecation('top', state)) {
+                console.log('go forward top');
+                await doStep('top');
+                continue;
+            }
+        }
+
+        if (!waysToGo(state)) {
+            console.log('go back');
+        }        
+
+        console.log('steps', steps);
+        break;
     }
     
     return ({ x: currentX, y: currentY });
 }
-
-
-// state(x: number, y: number): Promise<{
-//     top: boolean; // можно ли шагать вверх
-//     bottom: boolean; // можно ли шагать вниз
-//     left: boolean; // можно ли шагать влево
-//     right: boolean; // можно ли шагать вправо
-//     start: boolean; // ячейка - стартовая
-//     finish: boolean; // ячейка - финиш
-// }>;
